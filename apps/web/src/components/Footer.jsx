538ba66ef2634eedc351488ff764af5f33733547
RishabhDiwan2025 <rishabh@nexadvent.com>
2026-04-20 19:26:13 +0530

    Update Footer.jsx

--- apps/web/src/components/Footer.jsx
+++ apps/web/src/components/Footer.jsx
@@ -67,7 +67,7 @@ function Footer() {
               </li>
               <li className="flex items-center gap-2">
                 <Mail className="w-4 h-4 flex-shrink-0" />
-                <span>namanagg843@gmail.com</span>
+                <span>contact@printpackdelhi.in</span>
               </li>
             </ul>
           </div>
